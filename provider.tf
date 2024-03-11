51c09e6a174bf969d542f9677907c23f1ddfa11b
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-11 18:37:54 -0300

    ajuste provider

--- provider.tf
+++ provider.tf
@@ -1,9 +1,18 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "terraform-foodieflow-db"
     key    = "api/terraform.tfstate"
     region = "us-east-1"
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
