a863aad7a8f66407779d49149e67bb9878cd5f00
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-11 18:15:00 -0300

    ajuste de teste

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -18,7 +18,7 @@ resource "aws_lb_listener" "listener" {
 }
 
 resource "aws_lb_target_group" "group" {
-  name     = "alb-group${vat.projectName}"
+  name     = "alb-group-${vat.projectName}"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
